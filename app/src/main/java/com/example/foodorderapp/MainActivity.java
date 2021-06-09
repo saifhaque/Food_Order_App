2cf0c25244ee24808ce5d5a43a23f1302ff1d55e
saifhaque <haque.saif1905@gmail.com>
2021-06-09 16:50:54 +0530

    Toast message added

--- app/src/main/java/com/example/foodorderapp/MainActivity.java
+++ app/src/main/java/com/example/foodorderapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.foodorderapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Hi there", Toast.LENGTH_SHORT).show();
     }
 }
